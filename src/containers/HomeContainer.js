2778aa94551a536e45ccbf56a93342447bce56c3
tushaark7992 <tushaark7992@gmail.com>
2021-04-14 01:36:44 +0530

    added header and reducer

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,3 +1,15 @@
+import {connect} from 'react-redux'
 import Home from '../components/Home'
+import { addToCart } from "../services/Actions/action"
 
-export default Home;
+const mapStateToProps = state => ({
+    data: state.cardItems
+})
+
+const mapDispatchToProps = dispatch => ({
+    addToCartHandler: data => dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+
+// export default Home;
